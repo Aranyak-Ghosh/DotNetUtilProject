23f2b83908df6ff560fed928e71a57bc43359b3a
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2021-08-17 22:59:51 +0400

    Added required options for loggins

---
+++ CommonUtils/Options/LoggerOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Options
+{
+    public class StorageLoggerOptions : BaseOptions
+    {
+        public static string Accessor { get { return Key; } set { Key = "Logger:StorageAccount"; } }
+        public string ConnectionString { get; }
+    }
+
+    public class SQLLoggerOptions : BaseOptions
+    {
+        public static string Accessor { get { return Key; } set { Key = "Logger:SQL"; } }
+
+        public string ConnectionString { get; }
+    }
+}
