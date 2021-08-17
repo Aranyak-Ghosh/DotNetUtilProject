f4719c6434c282519b10ca5b9700a230da8d9301
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2021-08-17 22:49:11 +0400

    Added entity abstractions

---
+++ CommonUtils/Abstractions/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Abstractions.Entity
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedOn { get; protected set; }
+        public DateTime ModifiedOn { get; protected set; } = DateTime.UtcNow;
+        public Guid Id { get; protected set; }
+        public string CreatedBy { get; protected set; }
+        public string  ModifiedBy { get; protected set; }
+    }
+}
---
+++ CommonUtils/Abstractions/Entity/SoftDeleteableEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Abstractions.Entity
+{
+    class SoftDeleteableEntity : BaseEntity
+    {
+        public bool isDeleted { get; private set; }
+
+        public void DeleteEntity()
+        {
+            isDeleted = true;
+        }
+
+        public void RestoreEntity()
+        {
+            isDeleted = false;
+        }
+    }
+}
