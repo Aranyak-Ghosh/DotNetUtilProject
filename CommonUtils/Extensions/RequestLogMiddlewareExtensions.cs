a992a3b5d5eecea0cdedff99989a6352acc7d870
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2021-08-18 00:42:23 +0400

    Added extension method to configure request log middleware

---
+++ CommonUtils/Extensions/RequestLogMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CommonUtils.Logger;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Extensions
+{
+    public static class RequestLogMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLoggingMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<RequestLoggingMiddleware>();
+        }
+    }
+}
