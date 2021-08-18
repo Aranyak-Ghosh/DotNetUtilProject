f76b8e7ff38ef62a6dcd9437e3c58058356023f1
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2021-08-18 11:35:23 +0400

    Added context for SQL DB Logs

---
+++ CommonUtils/Logger/Provider/SQL/LogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CommonUtils.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Logger.Provider.SQL
+{
+    public class LogDbContext : DbContext
+    {
+        internal DbSet<RequestLogEntity> RequestLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
