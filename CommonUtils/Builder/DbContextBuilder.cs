4c1f913e247b60ff318d2b904b8fee15285077df
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2021-08-18 11:36:10 +0400

    Created generic provider for db context

---
+++ CommonUtils/Builder/DbContextBuilder.cs
@@ -0,0 +1,36 @@
+﻿using CommonUtils.Constants;
+using CommonUtils.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Builder
+{
+    public class DbContextBuilder<TContext> where TContext : DbContext, new()
+    {
+        private readonly DatabaseOptions _options;
+
+        public DbContextBuilder(IOptions<DatabaseOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public TContext Build()
+        {
+            var buildOptions = new DbContextOptionsBuilder<TContext>();
+
+            if(_options.DatabaseProvider == DatabaseProviders.MSSQL)
+                buildOptions.UseSqlServer(_options.ConnectionString, x=>x.UseNetTopologySuite());
+            if (_options.DatabaseProvider == DatabaseProviders.Postgres)
+                buildOptions.UseNpgsql(_options.ConnectionString);
+            if (_options.DatabaseProvider == DatabaseProviders.MySQL)
+                buildOptions.UseMySQL(_options.ConnectionString);
+
+            TContext context = (TContext) new DbContext(buildOptions.Options);
+            
+            return context;
+        }
+    }
+}
---
+++ CommonUtils/Constants/DatabaseProviders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonUtils.Constants
+{
+    public static class DatabaseProviders
+    {
+        public static readonly string MSSQL = "MSSQL";
+        public static readonly string Postgres = "NPGSql";
+        public static readonly string MySQL = "MySQL";
+    }
+}
